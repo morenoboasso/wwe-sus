276c7c057dfeef74f9e4772dd90b5b5b05c0ffcc
moreno-boasso <moreno.boasso@codeploy.it>
2024-08-06 17:23:27 +0200

    login styles

--- lib/style/color_style.dart
+++ lib/style/color_style.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 
 class ColorsBets {
-  static const Color orangeHD = Color(0xFFE87427);
-  static const Color yellowHD = Color(0xFFFECB45);
   static const Color blackHD = Color(0xFF080202);
   static const Color whiteHD = Color(0xFFFFFFFF);
-  static const Color greenHD = Color(0xFF71BC77);
 }
