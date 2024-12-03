6db44660ea3ab6bbd49439314ef78f3a396111be
moreno-boasso <moreno.boasso@codeploy.it>
2024-12-03 12:55:35 +0100

    UI changes

--- lib/widgets/match_card_info/wrestler_list.dart
+++ lib/widgets/match_card_info/wrestler_list.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import '../../style/text_style.dart';
 
 class WrestlerList extends StatelessWidget {
@@ -19,24 +18,15 @@ class WrestlerList extends StatelessWidget {
           style: MemoText.secondRowMatchInfo,
         ),
         const SizedBox(height: 16),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: wrestlers
-                  .take((wrestlers.length / 2).ceil())
-                  .map((wrestler) => Text('• $wrestler', style: MemoText.thirdRowMatchInfo))
-                  .toList(),
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: wrestlers
-                  .skip((wrestlers.length / 2).ceil())
-                  .map((wrestler) => Text('• $wrestler', style: MemoText.thirdRowMatchInfo))
-                  .toList(),
-            ),
-          ],
+        Wrap(
+          spacing: 20.0,
+          children: wrestlers
+              .map((wrestler) => Text(
+              '• $wrestler',
+              style: MemoText.thirdRowMatchInfo,
+              overflow: TextOverflow.fade,
+          ))
+              .toList(),
         ),
       ],
     );
